6de745bd9f0b24f6ac2a48a1018a96a5902a36da
air <air@MacBook-Air-air.local>
2019-12-01 20:57:38 +0100

    grudzien

--- PrzetwarzanieDanych/Big Data Nowe Uslugi zadania/recomendations.py
+++ PrzetwarzanieDanych/Big Data Nowe Uslugi zadania/recomendations.py
@@ -86,4 +86,27 @@ def topMatches(prefs, person, n=5, similarity=sim_distance):
     scores.sort()
     scores.reverse()
     return scores[0:n]
+
+def getRecommendations(prefs, person,similarity=sim_pearson):
+    totals={}
+    simSums={}
+    for other in prefs:
+        if other==person: continue
+        sim=similarity(prefs,person,other)
+        
+        if sim<=0: continue
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item]==0:
+                totals.setdefault(item,0)
+                totals[item]+=prefs[other][item]*sim
+                
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
+    rankings = [(total/simSums[item],item) for item,total in totals.items()]
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+        
+        
+
     
